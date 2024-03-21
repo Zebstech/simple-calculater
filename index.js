c0a108c706e08243248ad778db5b7ed74a19419a
Zebstech <zubair.saleem666@gmail.com>
2024-03-22 01:15:28 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter firstnumber", type: "number", name: "firstnumber" },
+    { message: "enter secondnumber", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operators",
+        choices: ["addition", "substraction", "multiplication", "division",],
+    },
+]);
+if (answer.operators === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operators === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operators === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operators === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
